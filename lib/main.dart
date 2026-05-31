f3f1c9a79b8b7df4eaa3a5e82b1ff8c6814257b1
Christian Alejandro Torres Hernández <minisirhc@gmail.com>
2026-05-31 17:49:29 -0600

    Commit inicial del proyecto

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+
+import 'app.dart';
+void main() {
+  runApp(DevicePreview(
+      enabled: kIsWeb,
+      builder: (_) => const App())
+  );
+}
+
